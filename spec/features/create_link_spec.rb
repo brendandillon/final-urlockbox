ac10f2f5607e4d91b5a22f65b891027658540da0
Brendan Dillon <br.lyle.dillon@gmail.com>
2017-01-09 10:47:49 -0700

    Add URL validation for links

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -13,6 +13,21 @@ RSpec.describe "can create links", :js => :true do
       expect(page).to have_text("Turing")
       expect(page).to have_text("http://turing.io")
     end
+  end
 
+  scenario "Not a valid URL", :js => :true do
+    log_in_user
+
+    visit "/"
+    fill_in "Title:", :with => "Turing"
+    fill_in "URL:", :with => "blargy blarg"
+    click_on "Add Link"
+
+    within('#links-list') do
+      expect(page).not_to have_text("Turing")
+      expect(page).not_to have_text("http://turing.io")
+    end
+
+    expect(page).to have_content('Url is not a valid URL')
   end
 end
